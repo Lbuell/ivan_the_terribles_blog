f8b83a8cf3c07f29635da6cbc9748a4ceb0fe286
lbuell <liamcbuell@gmail.com>
2014-02-25 21:09:07 -0800

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ IvanTheTerriblesBlog::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
-  config.assets.enabled = true.
+  config.assets.enabled = true
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
